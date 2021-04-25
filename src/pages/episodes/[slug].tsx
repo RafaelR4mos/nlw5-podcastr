df6720b89d21a253c209d0ce37585347c439b539
Rafael Ramos <rafaelramos0401@gmail.com>
2021-04-25 19:57:32 -0300

    Aplicação finalizada

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -3,10 +3,14 @@ import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import Image from 'next/image';
 import Link from 'next/link';
+import Head from 'next/head';
+import { useRouter } from 'next/router'
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { api } from '../../services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+
+import { usePlayer } from '../../contexts/PlayerContext';
 
 import styles from './episode.module.scss';
 
@@ -27,8 +31,13 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
+const { play } = usePlayer();
+
   return (
     <div className={styles.episode}>
+        <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
       <div className={styles.thumbnailContainer}>
         <Link href="/">
         <button type="button">
@@ -40,7 +49,7 @@ export default function Episode({ episode }: EpisodeProps) {
          src={episode.thumbnail}
          objectFit="cover" 
          />
-         <button type="button">
+         <button type="button" onClick={() => play(episode)}>
            <img src="/play.svg" alt="Tocar episódio"/>
          </button>
       </div>
@@ -58,8 +67,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: { 
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  })
+
   return {
-    paths: [],
+    paths: paths,
     fallback: 'blocking'
   }
 } 
